e4dd08c519f51c3a4645b0e5c2068880a22662bd
Natyes-ops <gurit.nat@bk.ru>
2023-02-23 13:15:08 +0300

    feat: Sort by rating

--- src/components/book-list/book-list.jsx
+++ src/components/book-list/book-list.jsx
@@ -18,10 +18,14 @@ export const BookList = (props) => {
 
     const [view, setView] = useState('grid');
     const [search, setSearch] = useState('');
+    const [sort, setSort] = useState(false);
+    const sortChange = () => setSort(!sort);
 
     const categoryBook = books.filter(book => currentCategory === 'Все книги' ? books : book.categories.includes(currentCategory))
 
     const searchBook = categoryBook.filter(book => book.title.toLowerCase().includes(search.toLowerCase()));
+
+    const sortBook = sort ? searchBook.sort((a,b) => a.rating - b.rating) : searchBook.sort((a,b) => b.rating - a.rating)
 
     const bookView = `books ${view}`;
 
@@ -37,7 +41,7 @@ export const BookList = (props) => {
 
     return(
         <main>
-        <Filter view={view} onClick={value => setView(value)} onChange={setSearch}/>
+        <Filter view={view} onClick={value => setView(value)} onChange={setSearch} sort={sort} sortChange={sortChange}/>
         {!categoryBook.length &&
             <div className='none-books h3 black40' data-test-id='empty-category'>
                 В этой категории книг ещё нет
@@ -49,7 +53,8 @@ export const BookList = (props) => {
             </div>
         }
         <div className={bookView}>
-            {!loading && categoryBook && searchBook && searchBook.map(book => 
+            {!loading && categoryBook && searchBook && sortBook.map(book => 
+                
                 <Link className='block' to={`/books/${category.find(i => i.name === book.categories[0]).path}/${book.id}`} key={book.id}>
                     <Book book={book} key={book.id} view={view} search={search}/>
                 </Link>
--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -9,9 +9,10 @@ import './filter.css';
 
 export const Filter = (props) => {
     const [search, setSearch] = useState(false);
-    const { onChange } = props;
+    const { onChange, sort, sortChange } = props;
     const toggleSearch = () => setSearch(!search);
     const activeSearch = () => search ? 'active' : '';
+    const sortClass = sort ? 'active' : '';
 
     return(
     <form action="" className="filter">
@@ -31,12 +32,12 @@ export const Filter = (props) => {
                 <img src={exit} alt='exit'/>
             </button>
         </div>
-        <div className="input">
-            <button type='button' className="filter-select black40 body-s" name="rating" id="rating">
+        <button type='button' className="filter-select  input" onClick={sortChange} data-test-id='sort-rating-button'>
+            <div type='button' className="black40 body-s" name="rating" id="rating" >
                 По рейтингу
-            </button>
-            <img className='filter-select__icon' src={ficon} alt='search'/>
-        </div>
+            </div>
+            <img className={`filter-select__icon ${sortClass}`}src={ficon} alt='search'/>
+        </button>
         <input className={props.view === 'grid' ? 'btn-f btn-grid active' : 'btn-f btn-grid'} type="button" onClick={() => props.onClick('grid')} data-test-id='button-menu-view-window'/>
         <input className={props.view === 'line' ? 'btn-f btn-line active' : 'btn-f btn-line'} type="button" onClick={() => props.onClick('line')} data-test-id='button-menu-view-list'/>
     </form>
