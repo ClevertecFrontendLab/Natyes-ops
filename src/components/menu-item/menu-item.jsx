ad3f8e4242f4dad9d78acd2347555ff527a979b5
Natyes-ops <gurit.nat@bk.ru>
2023-02-25 10:05:24 +0300

    feat: check close burger

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { Fragment, useCallback, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink, useLocation } from 'react-router-dom';
 
@@ -9,14 +9,14 @@ export const MenuItem = ({...props}) => {
     const root = useLocation();
     const activeLink = () => root.pathname ===  '/' ? 'active': '';
 
-    const { item, onClick, main, category } = props;
+    const { item, onClick, main, category, close } = props;
 
     const mWidth = 992;
     const cWidth = document.body.clientWidth;
     const checkMenuFirst = () => cWidth > mWidth ? 'navigation-books' : 'burger-books';
     const checkMenuItem = () => cWidth > mWidth ? `navigation-${item.path}` : `burger-${item.path}`;
-    const checkBurger = () => cWidth < mWidth ? onClick : null;
-
+    const checkMenuCount = () => cWidth > mWidth ? `navigation-book-count-for-${item.path}` : `burger-book-count-for-${item.path}`;
+    const checkBurger = () => cWidth > mWidth ? onClick : close;
     const count = useCallback(() => 
         books.filter(i => item && i.categories.includes(item.name)).length
     , [books, item]);
@@ -26,13 +26,16 @@ export const MenuItem = ({...props}) => {
             count()
         } 
     },[count, item])
+    console.log(checkBurger());
 
     return(<li className='menu-list__item body-l'>
     {
         main === 'main' ?
-        <NavLink to="/books/all" className={`title ${activeLink()}`} data-test-id={checkMenuFirst()} onClick={() => {checkBurger(); category('Все книги')}}><span className="title" >Все книги</span></NavLink>
+        <NavLink to="/books/all" className={`title ${activeLink()}`} data-test-id={checkMenuFirst()} onClick={() => {checkBurger()(); category('Все книги')}}><span className="title" >Все книги</span></NavLink>
         :
-        <NavLink to={`/books/${item.path}`} className='title' data-test-id={checkMenuItem()} onClick={() => {checkBurger(); category(item.name)}}><span className="title">{item.name} </span><span className="count body-s black40">{count()}</span></NavLink>
+        <Fragment>
+            <NavLink to={`/books/${item.path}`} className='title' data-test-id={checkMenuItem()} onClick={() => {checkBurger()(); category(item.name)}}><span className="title">{item.name}</span></NavLink><span className="count body-s black40" data-test-id={checkMenuCount()}>{count()}</span>
+        </Fragment>
     } 
 </li> 
 )
